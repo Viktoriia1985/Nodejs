411849b54deff2d9b82094075010085787bba1b4
Viktoriia Krat <flightsspb@gmail.com>
2021-09-04 13:34:11 +0300

    resolutions

--- classwork_1/appFilteredFolders.js
+++ classwork_1/appFilteredFolders.js
@@ -11,27 +11,26 @@ const newPathFolder = path.join(__dirname, 'filteredFolders/dirForEdit/newDir', 
 const newPathFolder2 = path.join(__dirname, 'filteredFolders/dirForEdit/newFolder', 'file333.txt');
 const renamePathFolder = path.join(__dirname, 'filteredFolders/dirForEdit/newFolder', 'newfile.txt');
 const movedFiles = path.join(__dirname, 'filteredFolders', 'movedFiles');
-const dirForEdit = path.join(filteredFolders, 'dirForEdit');
 
 
 fs.writeFile(newPathFolder, 'Hello, Gubby, Hello!!!', err => {
-    console.log(err);
+    if (err) console.log(err);
 })
 
 fs.writeFile(newPathFolder2, 'Goodbye, Gubby, Goodbye!!!', err => {
-    console.log(err);
-})
+    if (err) console.log(err);
+});
 
 fs.rename(newPathFolder, renamePathFolder, err => {
-    console.log(err);
-})
+    if (err) console.log(err);
+});
 
 fs.mkdir(movedFiles, { recursive: true }, err => {
     if (err) console.log(err);
 });
 
-function moveFiles(dirForEdit) {
 
+function moveFiles(dirForEdit) {
     fs.readdir(dirForEdit, (err, files) => {
         if (err) {
             console.log(err);
@@ -40,20 +39,20 @@ function moveFiles(dirForEdit) {
 
         files.forEach(file => {
             const oldPath = path.join(dirForEdit, file);
-           //console.log(oldPath);
 
             fs.stat(oldPath, (err1, stats) => {
                 if (err1) {
                     console.log(err1);
                     return;
                 }
-                console.log(stats.isDirectory(), 'isDirectory', file);
 
                 if (stats.isDirectory()) {
                     moveFiles(oldPath);
                     return;
                 }
+
                 const newPath = path.join(movedFiles, file);
+
                 fs.rename(oldPath, newPath, err2 => {
                     if (err2) {
                         console.log(err2);
@@ -63,6 +62,6 @@ function moveFiles(dirForEdit) {
         })
     })
 }
-moveFiles(dirForEdit);
+moveFiles(filteredFolders);
 
 
