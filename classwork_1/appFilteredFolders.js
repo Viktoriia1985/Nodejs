cff978b94589cedbbaf0842e980874986264e577
feden2906 <feden2906@gmail.com>
2021-09-04 22:57:32 +0300

    little fix

--- classwork_1/appFilteredFolders.js
+++ classwork_1/appFilteredFolders.js
@@ -7,34 +7,9 @@ const fs = require('fs');
 const path = require('path');
 
 const filteredFolders = path.join(__dirname, 'filteredFolders');
-const newPathFolder = path.join(__dirname, 'filteredFolders/dirForEdit/newDir', 'text1.txt');
-const newPathFolder2 = path.join(__dirname, 'filteredFolders/dirForEdit/newFolder', 'file333.txt');
-const renamePathFolder = path.join(__dirname, 'filteredFolders/dirForEdit/newFolder', 'newfile.txt');
-const movedFiles = path.join(__dirname, 'filteredFolders', 'movedFiles');
-const dirForEdit = path.join(filteredFolders, 'dirForEdit');
-
-
-fs.writeFile(newPathFolder, 'Hello, Gubby, Hello!!!', err => {
-    console.log(err);
-})
-
-fs.writeFile(newPathFolder2, 'Goodbye, Gubby, Goodbye!!!', err => {
-    console.log(err);
-})
-
-fs.rename(newPathFolder, renamePathFolder, err => {
-    console.log(err);
-})
-
-fs.mkdir(movedFiles, { recursive: true }, err => {
-    if (err) {
-        console.log(err);
-    }
-});
-
+const movedFiles = path.join(__dirname, 'movedFiles');
 
 function moveFiles(dirForEdit) {
-
     fs.readdir(dirForEdit, (err, files) => {
         if (err) {
             console.log(err);
@@ -43,20 +18,20 @@ function moveFiles(dirForEdit) {
 
         files.forEach(file => {
             const oldPath = path.join(dirForEdit, file);
-           //console.log(oldPath);
 
             fs.stat(oldPath, (err1, stats) => {
                 if (err1) {
                     console.log(err1);
                     return;
                 }
-                console.log(stats.isDirectory(), 'isDirectory', file);
 
                 if (stats.isDirectory()) {
                     moveFiles(oldPath);
                     return;
                 }
+
                 const newPath = path.join(movedFiles, file);
+
                 fs.rename(oldPath, newPath, err2 => {
                     if (err2) {
                         console.log(err2);
@@ -66,6 +41,7 @@ function moveFiles(dirForEdit) {
         })
     })
 }
-moveFiles(dirForEdit);
+
+moveFiles(filteredFolders);
 
 
